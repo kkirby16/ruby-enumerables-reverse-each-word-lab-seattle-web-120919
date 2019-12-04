7a154354928e159fded7ca8ad4e9befc71723020
Kevin Kirby <kkirby16@my.whitworth.edu>
2019-12-04 03:23:27 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,18 @@
+require 'pp'
+require 'pry'
+
+def reverse_each_word(sentence)
+  sentence.split.each do |word|
+    reversed_words = [word.reverse]
+  end
+end
+  
+
+#def reverse_each_word(sentence)
+#sentence.split.map {|word| word.reverse}.join(" ")
+#end
+
+#def reverse_each_word(sentence)
+ # separated_words = sentence.split 
+  #separated_words.map {|word| word.reverse}.join(" ")
+#end
