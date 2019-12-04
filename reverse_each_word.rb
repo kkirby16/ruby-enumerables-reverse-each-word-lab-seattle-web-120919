adf9bb1fa90c165058f7ab821c84e3ca733e3e7b
Kevin Kirby <kkirby16@my.whitworth.edu>
2019-12-04 03:29:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ require 'pry'
 def reverse_each_word(sentence)
   reversed_words = []
   sentence.split.each do |word|
-    reversed_words << [word.reverse]
+    reversed_words << word.reverse
   end
 end
   
